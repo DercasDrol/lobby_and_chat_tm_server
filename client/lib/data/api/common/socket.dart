4804ad592cab4071ee408410a55fcf6860b29896
Daniil Zubar <daniil.zubar@area9.dk>
2024-05-18 20:49:43 +0200

    v7. trying to fix build action

--- client/lib/data/api/common/socket.dart
+++ client/lib/data/api/common/socket.dart
@@ -2,9 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'package:mars_flutter/common/log.dart';
 import 'package:mars_flutter/data/api/constants.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'dart:html' as html;
 
 IO.Socket getNewSocketInstance(final String port) {
-  final host = kIsWeb ? Uri.base.origin : AUTH_CHAT_LOBBY_HOST;
+  final host = kIsWeb ? html.window.location.hostname : AUTH_CHAT_LOBBY_HOST;
   final fullUrl =
       host == 'localhost' ? 'ws://localhost:$port' : 'wss://$host:$port';
   final _socket = IO.io(fullUrl, <String, dynamic>{
